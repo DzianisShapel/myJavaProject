cd21a67c239b7bf85ad5cf6ea5ddfe4605117958
DzianisShapel <dzianis283@gmail.com>
2022-09-19 19:02:19 +0300

    to be continued hw3

---
+++ javaLessons/src/main/java/org/andersen/lab/lesson_3/EmployeeRunner.java
@@ -0,0 +1,8 @@
+package org.andersen.lab.lesson_3;
+
+public class EmployeeRunner {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Dzianis Shapel", "Tester", "dzianis.shapel@gmail.com", "+375297819364", 5000, 23);
+        employee.describeEmployee();
+    }
+}
