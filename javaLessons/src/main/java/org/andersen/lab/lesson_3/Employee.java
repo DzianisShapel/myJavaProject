09d3aa64094386fb11a70544e0012231fcadafda
DzianisShapel <dzianis283@gmail.com>
2022-09-20 09:46:32 +0300

    hw 3 done

--- javaLessons/src/main/java/org/andersen/lab/lesson_3/Employee.java
+++ javaLessons/src/main/java/org/andersen/lab/lesson_3/Employee.java
@@ -16,57 +16,30 @@ public class Employee {
         this.salary = salary;
         this.age = age;
     }
-
     public String getFullName() {
         return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
     }
 
     public String getPosition() {
         return position;
     }
 
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
     public String getEmail() {
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPhone() {
         return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
     }
 
     public int getSalary() {
         return salary;
     }
 
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
     public int getAge() {
         return age;
     }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void describeEmployee(){
+    public void describeEmployee() {
         System.out.println("Employee name is: " + getFullName() + ". Position is: " + getPosition() + ". Email: "
-        + getEmail() + ". Phone: " + getPhone() + ". Salary: " + getSalary() + ". Age: " + getAge() + ".");
+                + getEmail() + ". Phone: " + getPhone() + ". Salary: " + getSalary() + ". Age: " + getAge() + ".");
     }
 }
--- javaLessons/src/main/java/org/andersen/lab/lesson_3/EmployeeRunner.java
+++ javaLessons/src/main/java/org/andersen/lab/lesson_3/EmployeeRunner.java
@@ -2,7 +2,18 @@ package org.andersen.lab.lesson_3;
 
 public class EmployeeRunner {
     public static void main(String[] args) {
-        Employee employee = new Employee("Dzianis Shapel", "Tester", "dzianis.shapel@gmail.com", "+375297819364", 5000, 23);
-        employee.describeEmployee();
+
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Dzianis Shapel", "Tester", "dzianis.shapel@gmail.com", "375297819364", 5000, 23);
+        employees[1] = new Employee("Ivanov Ivan", "Developer", "ivanov.ivan@gmail.com", "375297819365", 6000, 40);
+        employees[2] = new Employee("Petrov Petr", "HR", "petrov.petr@gmail.com", "375297819366", 7000, 45);
+        employees[3] = new Employee("Sidorov Sidor", "BA", "sidorov.sidor@gmail.com", "375297819367", 8000, 49);
+        employees[4] = new Employee("Axmat Axmatov", "CEO", "axmat.axmatov@gmail.com", "375297819368", 9000, 55);
+
+        for (Employee employee : employees) {
+            if (employee.getAge() > 40) {
+                employee.describeEmployee();
+            }
+        }
     }
 }
